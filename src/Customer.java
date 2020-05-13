fc00be72c0e61a2370b978deadc472ba7b26c4ad
sebamomann <github@sebamomann.de>
2020-05-13 16:43:57 +0200

    generify

--- src/Customer.java
+++ src/Customer.java
@@ -4,7 +4,7 @@ import java.util.Vector;
 
 class Customer {
     private final String name;
-    private final Vector rentals = new Vector();
+    private final Vector<Rental> rentals = new Vector<>();
 
     public Customer(String newname) {
         name = newname;
@@ -21,13 +21,13 @@ class Customer {
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();
+        Enumeration<Rental> enum_rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
         while (enum_rentals.hasMoreElements()) {
             double thisAmount = 0;
-            Rental each = (Rental) enum_rentals.nextElement();
+            Rental each = enum_rentals.nextElement();
             //determine amounts for each line
             thisAmount = amountFor(each);
             // add frequent renter points
