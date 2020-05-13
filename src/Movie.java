21100b34fe843ee23b95d519357298d5577ee88e
sebamomann <github@sebamomann.de>
2020-05-13 16:40:23 +0200

    global formatting all final if possible

--- src/Movie.java
+++ src/Movie.java
@@ -2,19 +2,25 @@ public class Movie {
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
-    private String title;
+    private final String title;
     private int priceCode;
+
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
         priceCode = newpriceCode;
     }
+
     public int getPriceCode() {
         return priceCode;
     }
+
     public void setPriceCode(int arg) {
         priceCode = arg;
     }
-    public String getTitle (){
+
+    public String getTitle() {
         return title;
-    };
+    }
+
+    ;
 }
